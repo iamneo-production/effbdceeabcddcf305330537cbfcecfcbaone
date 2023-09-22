dd0bcf14890cdae7c5d13fdc71c6e84fee021a5a
Iamneo <techadmin@iamneo.ai>
2023-09-22 15:48:58 +0000

    Backup Commit

--- terraform/script.tf
+++ terraform/script.tf
@@ -1,13 +1,13 @@
 # Define the provider and region
 provider "aws" {
   region = "ap-northeast-1" # Tokyo region
-  access_key=""
-  secret_key=""
+  access_key="AKIAV4PYHDF5PADJN34L"
+  secret_key="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"
 }
 
 # Define the EC2 instance resource
 resource "aws_instance" "my_instance" {
-  ami           = "ami-0c55b159cbfafe1f0" # Ubuntu Server 20.04 AMI in Tokyo
+  ami           = "ami-0ed99df77a82560e6" # Ubuntu Server 20.04 AMI in Tokyo
   instance_type = "t2.micro"
 }
 
